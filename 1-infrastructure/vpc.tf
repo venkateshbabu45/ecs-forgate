221e8be9a2f55b151303e5dd4d2a9fed762d5c6d
venkateshbabu45 <39214689+venkateshbabu45@users.noreply.github.com>
2019-12-10 12:07:29 +0530

    Update vpc.tf

--- 1-infrastructure/vpc.tf
+++ 1-infrastructure/vpc.tf
@@ -10,83 +10,57 @@ resource "aws_vpc" "production-vpc" {
   cidr_block           = "${var.vpc_cidr}"
   enable_dns_hostnames = true
 
-  tags {
-    Name = "Production-VPC"
-  }
-}
+ }
 
 resource "aws_subnet" "public-subnet-1" {
   cidr_block        = "${var.public_subnet_1_cidr}"
   vpc_id            = "${aws_vpc.production-vpc.id}"
-  availability_zone = "eu-west-1a"
-
-  tags {
-    Name = "Public-Subnet-1"
-  }
+  availability_zone = "us-east-1a"
+  
 }
 
 resource "aws_subnet" "public-subnet-2" {
   cidr_block        = "${var.public_subnet_2_cidr}"
   vpc_id            = "${aws_vpc.production-vpc.id}"
-  availability_zone = "eu-west-1b"
-
-  tags {
-    Name = "Public-Subnet-2"
-  }
+  availability_zone = "us-east-1b"
+  
 }
 
 resource "aws_subnet" "public-subnet-3" {
   cidr_block        = "${var.public_subnet_3_cidr}"
   vpc_id            = "${aws_vpc.production-vpc.id}"
-  availability_zone = "eu-west-1c"
-
-  tags {
-    Name = "Public-Subnet-3"
-  }
+  availability_zone = "us-east-1c"
+  
 }
 
 resource "aws_subnet" "private-subnet-1" {
   cidr_block        = "${var.private_subnet_1_cidr}"
   vpc_id            = "${aws_vpc.production-vpc.id}"
-  availability_zone = "eu-west-1a"
-
-  tags {
-    Name = "Private-Subnet-1"
-  }
+  availability_zone = "us-east-1a"
 }
 
 resource "aws_subnet" "private-subnet-2" {
   cidr_block        = "${var.private_subnet_2_cidr}"
   vpc_id            = "${aws_vpc.production-vpc.id}"
-  availability_zone = "eu-west-1b"
+  availability_zone = "us-east-1b"
 
-  tags {
-    Name = "Private-Subnet-2"
-  }
 }
 
 resource "aws_subnet" "private-subnet-3" {
   cidr_block        = "${var.private_subnet_3_cidr}"
   vpc_id            = "${aws_vpc.production-vpc.id}"
-  availability_zone = "eu-west-1c"
+  availability_zone = "us-east-1c"
 
-  tags {
-    Name = "Private-Subnet-3"
-  }
 }
 
 resource "aws_route_table" "public-route-table" {
   vpc_id = "${aws_vpc.production-vpc.id}"
-  tags {
-    Name = "Public-Route-Table"
-  }
+ 
 }
 
 resource "aws_route_table" "private-route-table" {
   vpc_id = "${aws_vpc.production-vpc.id}"
-  tags {
-    Name = "Private-Route-Table"
-  }
+ 
 }
 
 resource "aws_route_table_association" "public-route-1-association" {
@@ -123,9 +97,7 @@ resource "aws_eip" "elastic-ip-for-nat-gw" {
   vpc                       = true
   associate_with_private_ip = "10.0.0.5"
 
-  tags {
-    Name = "Production-EIP"
-  }
+  
 
   depends_on = ["aws_internet_gateway.production-igw"]
 }
@@ -134,9 +106,7 @@ resource "aws_nat_gateway" "nat-gw" {
   allocation_id = "${aws_eip.elastic-ip-for-nat-gw.id}"
   subnet_id     = "${aws_subnet.public-subnet-1.id}"
 
-  tags {
-    Name = "Production-NAT-GW"
-  }
+ 
 
   depends_on = ["aws_eip.elastic-ip-for-nat-gw"]
 }
@@ -149,9 +119,7 @@ resource "aws_route" "nat-gw-route" {
 
 resource "aws_internet_gateway" "production-igw" {
   vpc_id = "${aws_vpc.production-vpc.id}"
-  tags {
-    Name = "Production-IGW"
-  }
+ 
 }
 
 resource "aws_route" "public-internet-igw-route" {
