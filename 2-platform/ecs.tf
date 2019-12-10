f02cdea999bd5435766cdb5e39717dd422b4abc3
venkateshbabu45 <39214689+venkateshbabu45@users.noreply.github.com>
2019-12-10 20:48:02 +0530

    Update ecs.tf

--- 2-platform/ecs.tf
+++ 2-platform/ecs.tf
@@ -35,7 +35,7 @@ resource "aws_alb_listener" "ecs_alb_https_listener" {
   ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
   certificate_arn   = "${aws_acm_certificate.ecs_domain_certificate.arn}"
 
-  "default_action" {
+  default_action {
     type              = "forward"
     target_group_arn  = "${aws_alb_target_group.ecs_default_target_group.arn}"
   }
